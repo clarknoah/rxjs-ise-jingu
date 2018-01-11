4c29c89c0b9b42567d094090ba397285bd48408b
clarknoah <noahbc08@gmail.com>
2018-01-11 13:26:16 -0500

    Got fromEvent Observable working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef , AfterViewInit} from '@angular/core';
+import {Observable} from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+  inputVar: ElementRef;
+
+  constructor(private el: ElementRef){
+
+  }
+
+  ngAfterViewInit(){
+    var input  = this.el.nativeElement.querySelector('#inputVar');
+    var observer = Observable.fromEvent(
+      input, 'keyup'
+    );
+    observer
+    .filter((data)=>{
+        return input.value==='aaa';
+    })
+    .subscribe(
+      (data)=>{
+        console.log(input.value);
+      }
+    );
+    console.log(input);
+  }
 }

34511e12db32828da5b251fd0baeb74db19b4c9c
clarknoah <noahbc08@gmail.com>
2018-03-16 17:06:01 -0400

    Initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClassesComponent } from './classes/classes.component';
+import { PatternsComponent } from './patterns/patterns.component';
+import { OperatorsService} from './operators.service';
+
+const routes: Routes = [
+  {path: '',redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'patterns', component:PatternsComponent},
+  {path: 'classes',component:ClassesComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClassesComponent,
+    PatternsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent ]
+  providers: [
+    OperatorsService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
